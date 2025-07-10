a13132c0ec3f6b1911e56dfc3ad7d8caec98ca82
lpanimesh27@gmail.com <lpanimesh27@gmail.com>
2025-07-10 05:29:32 +0000

    commit class 5 with test, packages ,macros

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -8,7 +8,8 @@
 with fct_reviews AS (
     SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM fct_reviews
+SELECT {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
+    AS review_id, * FROM fct_reviews
 where review_text is not null
 
 {% if is_incremental() %}
